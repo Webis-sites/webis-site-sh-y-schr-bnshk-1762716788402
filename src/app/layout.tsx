aeda02fd82198171ae9e1790b422a00e993f6755
Anass <146187264+nasaou@users.noreply.github.com>
2025-11-10 13:32:22 +0100

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'ש.י סחר בנשק',
+  description: 'חברת ש.י היא חברה חדשה וחדשנית בתחום הסחר הבינלאומי בנשק. החברה מתמחה בייבוא, ייצוא וסחר של אמצעי לחימה וציוד ביטחוני בין מדינות, ובאספקת פתרונות מתקדמים לגורמים ממשלתיים, ארגונים רשמיים וגורמי ביטחון במדינת ישראל ובעולם.
+ש.י פועלת על פי תקני בטיחות, פיקוח ורגולציה מחמירים, תוך הקפדה על שקיפות מלאה, אמינות ועמידה בכל דרישות החוק המקומיות והבינלאומיות.
+מטרת החברה היא לספק גישה יעילה, בטוחה ואמינה למערכות נשק וציוד ביטחוני איכותי, בשירות גורמי ביטחון לאומיים ובין־לאומיים.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="he" dir="rtl">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#141414" />
+      </head>
+      <body className={inter.className}>
+        <div className="flex min-h-screen flex-col">
+          {/* Navbar will be inserted here */}
+
+          {children}
+
+        </div>
+      </body>
+    </html>
+  );
+}
